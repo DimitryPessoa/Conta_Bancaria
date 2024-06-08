770af48cbf19bd618f502a00534e63272b00dbf2
Dimitry Pessoa <dimitry.pessoa@gmail.com>
2024-06-08 14:27:54 -0300

    agencia, conta e nome criados

---
+++ src/conta_bancaria/Account.java
@@ -0,0 +1,26 @@
+package conta_bancaria;
+
+public class Account {
+	
+	private String ag;
+	private String cc;
+	private String name;
+	
+	private static final int MAX_LENGTH = 20;
+	
+	public Account(String ag, String cc, String name) {
+		this.ag = ag;
+		this.cc = cc;
+		setName(name);
+		
+	}
+
+	public void setName(String name) {
+		if(name.length() > MAX_LENGTH) {
+			this.name = name.substring(0, MAX_LENGTH);
+		} else {
+			this.name=name;
+		}
+		System.out.println(this.name);
+	}
+}
--- src/conta_bancaria/App.java
+++ src/conta_bancaria/App.java
@@ -3,7 +3,20 @@ package conta_bancaria;
 public class App {
 	
 	public static void main(String[] args) {
-		System.out.print("Olá, mundo!");
+				
+		// create account (agency, account, name)
+		
+		Account account = new Account("001", "1234", "Usuario 1");
+		
+		Account account2 = new Account("002", "1234", "Usuario 2");
+	
+		
+		
+		// 		limite the name = 12 character
+		// withdraw value 
+		//  	no more than the total
+		// 	deposit
+		// inform the user of movement
 	}
 
 }
